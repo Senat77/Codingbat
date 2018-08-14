f61f7a85269d9366930a3f20f8b21e54bf0b7fd6
Sergii Senatorov <sergii.senatorov@gmail.com>
2018-08-14 17:03:25 +0300

    icyHot loneTeen startOz

---
+++ Warmup-1/src/startOz/Solution.java
@@ -0,0 +1,30 @@
+package startOz;
+
+/*
+Given a string, return a string made of the first 2 chars (if present), however include first char only if it is 'o'
+and include the second only if it is 'z', so "ozymandias" yields "oz".
+
+
+startOz("ozymandias") → "oz"
+startOz("bzoo") → "z"
+startOz("oxx") → "o"
+ */
+
+public class Solution
+{
+    public static void main(String[] args)
+    {
+        System.out.println(startOz("ozymandias"));
+        System.out.println(startOz("bzoo"));
+        System.out.println(startOz("oxx"));
+    }
+
+    public static String startOz(String str)
+    {
+        String res = "";
+        if(str == null) return res;
+        if(str.length() > 0 && str.charAt(0) == 'o') res = "o";
+        if(str.length() > 1 && str.charAt(1) == 'z') res += "z";
+        return res;
+    }
+}
